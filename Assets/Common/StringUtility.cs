8fec02f629fd2b73bc0669c0d62207afa2154742
manakam <manaka@rd6.so-net.ne.jp>
2017-10-24 09:03:49 +0900

    Gesture処理と2DのUnityChan画像を追加

---
+++ Assets/Common/StringUtility.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace Common {
+
+    public static class StringUtility {
+
+        private static StringBuilder _builder = new StringBuilder();
+
+        public static string Add(params string[] args) {
+            _builder.Length = 0;
+
+            for (int i = 0, len = args.Length; i < len; ++i) {
+                _builder.Append(args[i]);
+            }
+
+            return _builder.ToString();
+        }
+    }
+}
